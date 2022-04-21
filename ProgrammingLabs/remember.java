6687bc42ed0850f26393e4a8944ca7b76556af15
Pepegasus1234 <omegalul12334@gmail.com>
2022-04-21 21:44:55 +0500

    added gitignore

--- ProgrammingLabs/remember.java
+++
@@ -1,8 +0,0 @@
-public class remember {
-    public static void main(String args[]) {
-        int arr[] = {5, 6, 7, 8};
-        for (int x : arr) {
-            System.out.println(x);
-        } 
-    }    
-}
